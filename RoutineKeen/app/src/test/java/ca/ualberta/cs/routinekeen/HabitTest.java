af2e329e6555d787bec04d82c6ae09c53cd39a43
hapdag <syhe@ualberta.ca>
2017-10-20 18:53:37 -0600

    inital commit with habit unit test

---
+++ RoutineKeen/app/src/test/java/ca/ualberta/cs/routinekeen/HabitTest.java
@@ -0,0 +1,29 @@
+package ca.ualberta.cs.routinekeen;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class HabitTest{    @Test
+    public void testHabit(){
+    String habitTitle = "test habit title", habitReason = "test habit reason";
+    Date testDate = new Date();
+    Habit testHabit = new Habit(habitTitle,habitReason,testDate);
+
+    assertTrue(testHabit.getHabitTitle().equal(habitTitle));
+    assertTrue(testHabit.getHabitReason().equal(habitReason));
+    assertTrue(testHabit.getHabitStartDate().euqal(testDate));
+
+    String newTitle = "new title";
+    String newReason = "new reasoning";
+
+    testHabit.setTitle = newTitle;
+    testHabit.setReason = newReason;
+
+    assertTrue(testHabit.getHabitReason().equal(newTitle));
+    assertTrue(testHabit.getHabitStartDate().euqal(newReason));
+
+    }
+}
