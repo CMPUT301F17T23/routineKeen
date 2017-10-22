769a0022b98469c7fda35dae49d63ee7a2934948
vprice <31709840+vprice@users.noreply.github.com>
2017-10-22 00:47:12 -0600

    fixed *maybe

--- RoutineKeen/app/src/test/java/ca/ualberta/cs/routinekeen/FollowerListTest.java
+++ RoutineKeen/app/src/test/java/ca/ualberta/cs/routinekeen/FollowerListTest.java
@@ -11,12 +11,16 @@ public class HabitUserListTest {
     User testFollower = new User(testUniqueFollowerName);
     assertTrue(testFollower.getUserName().equal(testUniqueFollowerName));
     
-    FollowerList testFollowerList = new FollowerList();
-    testFollowerList.add(testFollower);
-    assertTrue(testFollowerList.getUser(testFollower).equal(testFollower);
+    String testUniqueFollowerName = "Test follower name";
+    User testFollower = new User(testUniqueFollowerName);
+    assertTrue(testFollower.getUserName().equal(testUniqueFollowerName));
     
-    testFollowerList.remove(testFollower);
-    assertEquals(testFollowerList.size(), 0);
+    testUser.getFollowerList().add(testFollower);
+    assertTrue(testUser.getFollowersList().getFollower(testUniqueFollowerName).equal(testUniqueFollowerName));
+    
+    testUser.getFollowerList().remove(testFollower);
+    assertEquals(testUser.getFollowerList.size(), 0);
+    
   }
   
 }
