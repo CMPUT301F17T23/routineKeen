053eabfa2d82bc66ae2c64865602900c412f1368
hapdag <syhe@ualberta.ca>
2017-10-21 23:46:05 -0600

    added HabitEvent test with some tests

---
+++ RoutineKeen/app/src/test/java/ca/ualberta/cs/routinekeen/HabitEventTest.java
@@ -0,0 +1,20 @@
+package ca.ualberta.cs.routinekeen;
+import org.junit.Test;
+import java.util.Date;
+import static org.junit.Assert.*;
+public class HabitEventTest {
+    public void testHabitEvent() throws Exception {
+        String testHabitEventTitle = "Test HabitEvent title";
+        String testHabitComment = "Test HabitEvent comment";
+        HabitEvent testHabitEvent = new HabitEvent(testHabitEventTitle,testHabitComment);
+        assertTrue(testHabitEvent.getEventTitle().equal(testHabitEventTitle));
+        assertTrue(testHabitEvent.getEventComment().equal(testHabitComment));
+
+        String newHabitEventTitle = "New HabitEvent title";
+        String newHabitEventComment = "New HabitEvent comment";
+        testHabitEvent.setEventTitle(newHabitEventTitle);
+        testHabitEvent.setEventComment(newHabitEventComment);
+        assertTrue(testHabitEvent.getEventTitle().equal(newHabitEventTitle));
+        assertTrue(testHabitEvent.getEventComment().equal(newHabitComment));
+    }
+}
---
+++ RoutineKeen/app/src/test/java/ca/ualberta/cs/routinekeen/HabitHistoryTest.java
@@ -0,0 +1,9 @@
+package ca.ualberta.cs.routinekeen;
+import org.junit.Test;
+import java.util.Date;
+import static org.junit.Assert.*;
+public class HabitListControllerTest {
+    public void testHabitListController() throws Exception {
+
+    }
+}
