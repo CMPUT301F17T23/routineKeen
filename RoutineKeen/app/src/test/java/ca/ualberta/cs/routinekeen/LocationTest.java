de36fbea1be01b1972b4fdc5b5c4b88b2699fde6
vprice <31709840+vprice@users.noreply.github.com>
2017-10-22 01:03:53 -0600

    Check if correct

---
+++ RoutineKeen/app/src/test/java/ca/ualberta/cs/routinekeen/LocationTest.java
@@ -0,0 +1,22 @@
+package ca.ualberta.cs.routinekeen;
+import org.junit.Test;
+import java.util.Date;
+import static org.junit.Assert.*;
+
+public class testLocation {
+  @Test
+  
+  public void testLocation() throws Exception {
+    String testHabitEventTitle = "Test HabitEvent title";
+    HabitEvent testHabitEvent = new HabitEvent(testHabitEventTitle);
+    
+    String testLocation = "Test Location";
+    testHabitEvent.addLocation(testLocation);
+    assertTrue(testHabitEvent.getLocation().equal(testLocation);
+    
+    testHabitEvent.removeLocation(testLocation);
+    assertNull(testHabitEvent.getLocation());
+  
+  }
+  
+}
