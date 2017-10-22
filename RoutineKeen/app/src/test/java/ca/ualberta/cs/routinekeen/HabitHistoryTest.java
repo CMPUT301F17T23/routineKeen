aab51a0d4a161eb5edd03cad3d7264d432a3b3d9
hapdag <syhe@ualberta.ca>
2017-10-22 00:28:19 -0600

    added test for HabitHistory

--- RoutineKeen/app/src/test/java/ca/ualberta/cs/routinekeen/HabitHistoryTest.java
+++ RoutineKeen/app/src/test/java/ca/ualberta/cs/routinekeen/HabitHistoryTest.java
@@ -1,9 +1,25 @@
 package ca.ualberta.cs.routinekeen;
 import org.junit.Test;
+
+import java.util.Collection;
 import java.util.Date;
 import static org.junit.Assert.*;
-public class HabitListControllerTest {
-    public void testHabitListController() throws Exception {
+public class HabitHistoryTest{
+    public void testHabitHistory() throws Exception {
+        String testHabitEventTitle = "Test HabitEvent title";
+        String testHabitComment = "Test HabitEvent comment";
+        HabitEvent testHabitEvent = new HabitEvent(testHabitEventTitle,testHabitComment);
+        HabitHistory testHabitHistory = new HabitHistory();
 
+        testHabitHistory.addHabitEvent(testHabitEvent);
+        assertTrue(testHabitHistory.getHabitEvent(testHabitEventTitle).equal(testHabitEvent));
+
+        testHabitHistory.removeHabitEvent(testHabitEventTitle);
+        assertTrue(testHabitHistory.size().equal(0));
+
+        testHabitHistory.addHabitEvent(testHabitEvent);
+        String testHabitFilter = "test filter Habit type";
+        Collection<HabitEvent> testFilteredList = testHabitHistory.getFilterdList(testHabitFilter);
+        assertTrue(testFilteredList.get(testHabitEventTitle).equal(testHabitEvent));
     }
 }
