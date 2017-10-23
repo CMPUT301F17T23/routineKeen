52ab5da750e3176345398eb3eb6f715daab4b02c
Toluwanimi I. Akindele <tiakindele11@gmail.com>
2017-10-23 13:28:21 -0600

    Habit class added for HabitTest

--- RoutineKeen/app/src/main/java/ca/ualberta/cs/routinekeen/Models/Habit.java
+++ RoutineKeen/app/src/main/java/ca/ualberta/cs/routinekeen/Models/Habit.java
@@ -1,8 +1,31 @@
 package ca.ualberta.cs.routinekeen.Models;
 
+import java.util.Date;
+
 /**
  * Created by hughc on 2017-10-23.
  */
 
 public class Habit {
+
+    public String setTitle;
+    public String setReason;
+
+    public Habit(String habitTitle, String habitReason, Date testDate) {
+    }
+
+    public String getTitle() {
+        String title = null;
+        return title;
+    }
+
+    public String getReason() {
+        String reason = null;
+        return reason;
+    }
+
+    public Object getStartDate() {
+        Object startDate = null;
+        return startDate;
+    }
 }
