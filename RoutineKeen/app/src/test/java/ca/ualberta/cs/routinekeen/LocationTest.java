16a983563f5901ccd821ee7508a17e57ce786cb7
Hugh Craig <hdc@ualberta.ca>
2017-10-23 14:54:42 -0600

    Habit location class created fixed location test

--- RoutineKeen/app/src/test/java/ca/ualberta/cs/routinekeen/LocationTest.java
+++ RoutineKeen/app/src/test/java/ca/ualberta/cs/routinekeen/LocationTest.java
@@ -2,6 +2,7 @@ package ca.ualberta.cs.routinekeen;
 import org.junit.Test;
 
 import ca.ualberta.cs.routinekeen.Models.HabitEvent;
+import ca.ualberta.cs.routinekeen.Models.HabitLocation;
 
 import static org.junit.Assert.*;
 
@@ -12,13 +13,9 @@ public class LocationTest {
     String testHabitEventTitle = "Test HabitEvent title";
     HabitEvent testHabitEvent = new HabitEvent(testHabitEventTitle);
 
-    String testLocation = "Test Location";
-    testHabitEvent.addLocation(testLocation);
-    assertTrue(testHabitEvent.getLocation().equals(testLocation);
-
-    testHabitEvent.removeLocation(testLocation);
-    assertTrue(testHabitEvent.getLocation().equals("");
-  
+    HabitLocation testLocation = new HabitLocation("Test Location");
+    testHabitEvent.setLocation(testLocation);
+    assertTrue(testHabitEvent.getLocation().equals(testLocation));
   }
   
 }
