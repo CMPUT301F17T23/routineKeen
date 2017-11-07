2aaf67d51ae84fb8c05a795b577e93801c30f50c
Toluwanimi I. Akindele <tiakindele11@gmail.com>
2017-11-06 19:34:13 -0700

    user menu layout edit 1 complete

---
+++ RoutineKeen/app/src/main/java/ca/ualberta/cs/routinekeen/Views/UserMenu.java
@@ -0,0 +1,24 @@
+package ca.ualberta.cs.routinekeen.Views;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ca.ualberta.cs.routinekeen.R;
+
+/**
+ * Created by tiakindele on 2017-11-06.
+ */
+
+public class UserMenu extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_menu);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
