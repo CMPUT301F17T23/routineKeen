f1fb3b6033c0056c1fa94ce250929546ff934b15
vprice <miks.15v@gmail.com>
2017-11-08 20:05:43 -0700

    added some stuff for history

---
+++ RoutineKeen/app/src/main/java/ca/ualberta/cs/routinekeen/Controllers/HabitHistoryController.java
@@ -0,0 +1,15 @@
+package ca.ualberta.cs.routinekeen.Controllers;
+
+import java.util.ArrayList;
+
+import ca.ualberta.cs.routinekeen.Models.HabitHistory;
+import ca.ualberta.cs.routinekeen.Models.HabitEvent;
+import ca.ualberta.cs.routinekeen.Models.Photo;
+/**
+ * Created by Mikee V on 2017-11-08.
+ */
+
+public class HabitHistoryController {
+    private ArrayList<HabitEvent> habitHistory = new ArrayList<HabitEvent>();
+
+}
