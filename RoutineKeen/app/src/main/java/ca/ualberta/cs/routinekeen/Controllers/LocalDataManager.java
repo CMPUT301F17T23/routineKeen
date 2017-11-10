79069e30e601d2f7a93820c60ea6c8c19dba0082
Hugh Craig <hdc@ualberta.ca>
2017-11-09 19:31:37 -0700

    WE BACK ON 2.3 BOIZZZ
    ANDROID STUDIO 3.0 SUCKS MAN

--- RoutineKeen/app/src/main/java/ca/ualberta/cs/routinekeen/Controllers/LocalDataManager.java
+++ RoutineKeen/app/src/main/java/ca/ualberta/cs/routinekeen/Controllers/LocalDataManager.java
@@ -43,15 +43,15 @@ public class LocalDataManager {
         return localDataManager;
     }
 
-    public User loadLocalUserData(HabitList habitList){
-        SharedPreferences settings = context.getSharedPreferences(prefFile,Context.MODE_PRIVATE);
-        String habitListData = settings.getString(userName,"");
-        if (habitListData.equals("")) {
-            throw new RuntimeException("no such user exist");
-        }else{
-            return offlineDataFromString(); //FIRST CONVERT STRING TO JSON, THEN JSON TO OBJECT USING GSON
-        }
-    }
+//    public User loadLocalUserData(HabitList habitList){
+//        SharedPreferences settings = context.getSharedPreferences(prefFile,Context.MODE_PRIVATE);
+//        String habitListData = settings.getString(userName,"");
+//        if (habitListData.equals("")) {
+//            throw new RuntimeException("no such user exist");
+//        }else{
+//            return offlineDataFromString(); //FIRST CONVERT STRING TO JSON, THEN JSON TO OBJECT USING GSON
+//        }
+//    }
 
     public void saveHabitList(HabitList habitList){
 
