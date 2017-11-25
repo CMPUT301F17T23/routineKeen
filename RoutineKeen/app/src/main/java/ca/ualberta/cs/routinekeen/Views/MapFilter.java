229beece36905cfa3b64f05b529968b0055dd309
Toluwanimi I. Akindele <tiakindele11@gmail.com>
2017-11-24 19:25:19 -0700

    added buttons

---
+++ RoutineKeen/app/src/main/java/ca/ualberta/cs/routinekeen/Views/MapFilter.java
@@ -0,0 +1,36 @@
+package ca.ualberta.cs.routinekeen.Views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import ca.ualberta.cs.routinekeen.R;
+
+/**
+ * Created by tiakindele on 2017-11-24.
+ */
+
+public class MapFilter extends AppCompatActivity{
+    Button cancel;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_filter);
+        cancel = (Button)findViewById(R.id.filterCancelBtn);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MapFilter.this, MapsActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    public void saveFilter(View view) {
+        Toast.makeText(this, "Save Clicked!", Toast.LENGTH_SHORT).show();
+    }
+}
--- RoutineKeen/app/src/main/java/ca/ualberta/cs/routinekeen/Views/MapsActivity.java
+++ RoutineKeen/app/src/main/java/ca/ualberta/cs/routinekeen/Views/MapsActivity.java
@@ -1,7 +1,10 @@
 package ca.ualberta.cs.routinekeen.Views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,6 +18,7 @@ import ca.ualberta.cs.routinekeen.R;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    Button options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        options = (Button)findViewById(R.id.map_options);
+        options.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MapsActivity.this, MapFilter.class);
+                startActivity(i);
+            }
+        });
     }
 
 
