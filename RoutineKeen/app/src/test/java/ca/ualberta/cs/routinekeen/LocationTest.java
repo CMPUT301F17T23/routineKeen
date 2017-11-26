bcb5674c09d99ca722a955b048fc9820d77784d5
Hugh Craig <hdc@ualberta.ca>
2017-11-25 20:59:12 -0700

    some testing for elastic search

--- RoutineKeen/app/src/test/java/ca/ualberta/cs/routinekeen/LocationTest.java
+++ RoutineKeen/app/src/test/java/ca/ualberta/cs/routinekeen/LocationTest.java
@@ -1,22 +1,22 @@
 package ca.ualberta.cs.routinekeen;
+import com.google.android.gms.maps.model.LatLng;
+
 import org.junit.Test;
 
 import ca.ualberta.cs.routinekeen.Models.HabitEvent;
-import ca.ualberta.cs.routinekeen.Models.HabitLocation;
 
 import static org.junit.Assert.*;
 
 public class LocationTest {
-  @Test
-  
-  public void LocationTest() throws Exception {
-    String testHabitEventTitle = "Test HabitEvent title";
-    String testHabitType = "test habit type";
-    HabitEvent testHabitEvent = new HabitEvent(testHabitEventTitle,testHabitType);
+    @Test
+    public void LocationTest() throws Exception {
+        String testHabitEventTitle = "Test HabitEvent title";
+        LatLng testLocation = new LatLng(-41.2, 42.5);
+        String testHabitType = "test habit type";
+        HabitEvent testHabitEvent = new HabitEvent(testHabitEventTitle,testHabitType);
 
-    HabitLocation testLocation = new HabitLocation("Test Location");
-    testHabitEvent.setLocation(testLocation);
-    assertTrue(testHabitEvent.getLocation().equals(testLocation));
-  }
+        testHabitEvent.setLocation(testLocation);
+        assertTrue(testHabitEvent.getLocation().equals(testLocation));
+    }
   
 }
