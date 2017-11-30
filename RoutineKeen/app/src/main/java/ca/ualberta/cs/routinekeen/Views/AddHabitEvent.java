52be7af6ba79ae878670fcb94d83f16bee7bd93c
aridgway120 <aridgway@ualberta.ca>
2017-11-29 18:29:28 -0700

    Working images with compression/cropping to square

--- RoutineKeen/app/src/main/java/ca/ualberta/cs/routinekeen/Views/AddHabitEvent.java
+++ RoutineKeen/app/src/main/java/ca/ualberta/cs/routinekeen/Views/AddHabitEvent.java
@@ -1,11 +1,14 @@
 package ca.ualberta.cs.routinekeen.Views;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +17,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.apache.commons.lang3.ObjectUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -28,8 +33,10 @@ import ca.ualberta.cs.routinekeen.R;
 public class AddHabitEvent extends AppCompatActivity {
 
     protected static final int REQUEST_SELECT_IMAGE = 1;
+    protected static final int REQUEST_PERMISSION_READ_EXTERNAL_STORAGE = 2;
+
     protected static final int IMAGE_MAX_BYTES = 65536;
-    protected static final int LENGTH = 256;
+    protected static final int LENGTH = (int) Math.floor(Math.sqrt(IMAGE_MAX_BYTES));
     private ImageView eventImageView;
     private Button selectImageButton;
 
@@ -75,37 +82,60 @@ public class AddHabitEvent extends AppCompatActivity {
     }
 
     public void selectImage() {
-        Intent intent = new Intent(Intent.ACTION_PICK,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent, REQUEST_SELECT_IMAGE);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                REQUEST_PERMISSION_READ_EXTERNAL_STORAGE);
+
+        //Intent intent = new Intent(Intent.ACTION_PICK,
+        //        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        //startActivityForResult(intent, REQUEST_SELECT_IMAGE);
 
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, REQUEST_SELECT_IMAGE);
+                } else {
+                    // permission denied, don't open gallery
+                }
+        }
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d("AHELog", "Check1");
         switch (requestCode) {
             case REQUEST_SELECT_IMAGE:
-                Log.d("AHELog", "Check2");
                 if (resultCode == RESULT_OK) {
                     Uri imageUri = data.getData();
-                    File f = new File(imageUri.getPath());
-                    String imagePath = f.getAbsolutePath();
 
-                    Bitmap bitmap;
-                    Log.d("AHELog", "Check3");
+                    try {
+                        // get selected image in bitmap form
+                        InputStream image_stream = getContentResolver().openInputStream(imageUri);
+                        Bitmap selectedImage = BitmapFactory.decodeStream(image_stream);
 
-                    //InputStream image_stream = getContentResolver().openInputStream(imageUri);
-                    Log.d("AHELog", "Check4");
-                    bitmap = ThumbnailUtils.extractThumbnail(BitmapFactory.decodeFile(imagePath), LENGTH, LENGTH);
-                    Log.d("AHELog", "Check5");
-                    //if (bitmap.getByteCount() >= IMAGE_MAX_BYTES);
-                    Log.d("AHELog", "Check6");
-                    eventImageView.setImageBitmap(bitmap);
-                    Log.d("AHELog", "Check7");
-                    // Thumbnails
-                    // Taken from: http://www.rogerethomas.com/blog/generating-square-cropped-thumbnails-in-android-java
-                    // on Nov 25, 2017
+                        // convert selected image to thumbnail smaller than IMAGE_MAX_BYTES
+                        Bitmap thumbImage = ThumbnailUtils.extractThumbnail(selectedImage,
+                                LENGTH, LENGTH);
+                        // Taken from: http://www.rogerethomas.com/blog/generating-square-cropped-thumbnails-in-android-java
+                        // on Nov 25, 2017
+
+                        // display new image in imageview
+                        eventImageView.setImageBitmap(thumbImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                        Log.d("AHELog", "FNF except");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Log.d("AHELog", "IO except");
+                    } catch (Exception e) {
+                        Log.d("AHELog", "Unknown except");
+                        Log.e("AHELog", "unknown", e);
+                    }
                 }
         }
     }
