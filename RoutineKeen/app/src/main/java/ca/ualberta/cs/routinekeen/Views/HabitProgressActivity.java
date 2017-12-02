f36d35618e48a34f6defe721e01f01f1ead1a417
hapdag <syhe@ualberta.ca>
2017-12-01 18:52:02 -0700

    finished the progress check

--- RoutineKeen/app/src/main/java/ca/ualberta/cs/routinekeen/Views/HabitProgressActivity.java
+++ RoutineKeen/app/src/main/java/ca/ualberta/cs/routinekeen/Views/HabitProgressActivity.java
@@ -3,6 +3,7 @@ package ca.ualberta.cs.routinekeen.Views;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -10,9 +11,12 @@ import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import com.google.common.primitives.Booleans;
 
+import org.w3c.dom.Text;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
@@ -35,61 +39,69 @@ public class HabitProgressActivity extends AppCompatActivity {
     private long dateRange;
     private Integer scheduleGoal = 0, scheduleCompleted = 0;
     private ProgressBar scheduledProgressBar;
+    private Button doneButton;
+    private TextView scheduledProgress;
 
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_progress);
         IOManager.initManager(getApplicationContext());
         scheduledProgressBar = (ProgressBar) findViewById(R.id.scheduledProgressBar);
+        scheduledProgress = (TextView) findViewById(R.id.scheduledProgressTextView);
         habitTypeTextView = (TextView) findViewById(R.id.habitTypeTextView);
         totalEventTextView = (TextView) findViewById(R.id.totalEventsCount);
         Bundle data = getIntent().getExtras();
         Bundle habitData = data.getBundle("habit");
         Habit habit = HabitListController.getHabitList().getHabitByPosition(habitData.getInt("position"));
-        String string = "Habit type: "+ habit.getHabitTitle();
+        String string = "For your habit " + habit.getHabitTitle() + ", your current progress is:";
         habitTypeTextView.setText(string);
 
         ArrayList<HabitEvent> filteredList = new ArrayList<>();
-        for (HabitEvent event : HabitHistoryController.getHabitHistory().getEvents()){
-            if (event.getEventHabitType().equals(habit.getHabitTitle())){
+        for (HabitEvent event : HabitHistoryController.getHabitHistory().getEvents()) {
+            if (event.getEventHabitType().equals(habit.getHabitTitle())) {
                 filteredList.add(event);
             }
         }
+
+        HashMap<String,HabitEvent> filteredMap = new HashMap<>();
+
+        for(HabitEvent event : filteredList){
+            String date = new SimpleDateFormat("yyyyMMdd").format(event.getDate());
+            if(!filteredMap.containsKey(date)){
+                filteredMap.put(date,event);
+            }
+        }
+
         totalEventTextView.setText(Integer.toString(filteredList.size()));
         Date startDate = habit.getStartDate();
         Date curDate = new Date();
-        ArrayList<Date> betweenDates = new ArrayList<Date>();
-        betweenDates = getBetweenDates(startDate,curDate);
+        ArrayList<Date> betweenDates = getBetweenDates(startDate, curDate);
         ArrayList<String> dates = habit.getScheduledHabitDays();
         Multimap<String, Date> dateMap = HashMultimap.create();
-        for(Date date: betweenDates){
+        for (Date date : betweenDates) {
             String key = new SimpleDateFormat("EE").format(date);
-            dateMap.put(key,date);
+            dateMap.put(key, date);
         }
 
-        for(String day : dates){
+        for (String day : dates) {
             scheduleGoal += dateMap.get(day).size();
-            if(dateMap.get(day) != null){
-                for(Date date : dateMap.get(day)){
-                    for(HabitEvent event : filteredList){
-                        if (sameDay(date,event.getDate())){
-                            scheduleCompleted++;
-                        }
+            for (Date date : dateMap.get(day)){
+                String tempDate = new SimpleDateFormat("yyyyMMdd").format(date);
+                for( String key : filteredMap.keySet()){
+                    if ( tempDate.equals(key) ){
+                        scheduleCompleted++;
                     }
                 }
             }
+
+                double x = (double) scheduleCompleted / (double) scheduleGoal;
+                scheduledProgressBar.setProgress((int) (x * 100));
         }
-        scheduledProgressBar.setProgress((scheduleCompleted/scheduleGoal)*100);
+
+        scheduledProgress.setText(Integer.toString(scheduleCompleted) + " day(s) out of the "
+                + Integer.toString(scheduleGoal) + " day(s) scheduled");
     }
 
-    private boolean sameDay(Date date1, Date date2) {
-        Calendar cal1 = Calendar.getInstance();
-        Calendar cal2 = Calendar.getInstance();
-        cal1.setTime(date1);
-        cal2.setTime(date2);
-        return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
-                cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR);
-    }
 
     //get a list of dates between two java dates
     //https://stackoverflow.com/questions/2689379/how-to-get-a-list-of-dates-between-two-dates-in-java
@@ -105,5 +117,18 @@ public class HabitProgressActivity extends AppCompatActivity {
         return dates;
     }
 
-}
 
+    private boolean sameDay1(String day, Date date) {
+        String eventDate = new SimpleDateFormat("EE").format(date);
+        return eventDate.equals(day);
+    }
+
+    private boolean sameDay2(Date day1, Date day2) {
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal1.setTime(day1);
+        cal2.setTime(day2);
+        return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
+                cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR);
+    }
+}
